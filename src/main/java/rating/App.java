4308574d2cc993cbaaf00280d986981909e36c96
Paris Tao <paristao1989@gmail.com>
2015-06-17 15:16:31 +0800

    初始化评分考核系统
    初始化评分考核系统201500617

---
+++ src/main/java/rating/App.java
@@ -0,0 +1,10 @@
+package rating;
+/**
+ * Description: 
+ * @author Paris
+ * @date Jun 17, 20153:13:22 PM
+ * @version beta
+ */
+public class App {
+
+}
---
+++ src/test/java/rating/AppTest.java
@@ -0,0 +1,10 @@
+package rating;
+/**
+ * Description: 
+ * @author Paris
+ * @date Jun 17, 20153:14:05 PM
+ * @version beta
+ */
+public class AppTest {
+
+}
