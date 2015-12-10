be6b54bed34eda50177a3e959992034a33d819d1
Paris Tao <paristao1989@gmail.com>
2015-12-10 17:11:31 +0800

    文件路径空格问题

--- src/main/java/com/chinesedreamer/rating/common/utils/LicenseUtil.java
+++ src/main/java/com/chinesedreamer/rating/common/utils/LicenseUtil.java
@@ -1,13 +1,31 @@
 package com.chinesedreamer.rating.common.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LicenseUtil {
+	
+	private static Logger logger = LoggerFactory.getLogger(LicenseUtil.class);
+	
 	/**
 	 * 获得授权文件绝对路径
 	 * @return
 	 */
 	public static String getLicensePath() {
 		String applicationPath = LicenseUtil.class.getClassLoader().getResource("application.properties").getPath();
+		logger.info("applicationPath:{}",applicationPath);
 		int classIndex = applicationPath.lastIndexOf("rating");
-		return applicationPath.substring(0, classIndex) + "rating.license";
+		String path = applicationPath.substring(0, classIndex) + "rating.license";
+		if (path.contains("%20")) {
+			try {
+				path = URLDecoder.decode(path, "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				logger.error("{}",e);
+			}
+		}
+		return path;
 	}
 }
